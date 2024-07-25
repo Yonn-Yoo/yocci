e7d87b381b6c97f08f0320a1c439da25acb3547f
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-07-25 15:14:24 +0900

    refactor: remove unnecessary code

--- src/contexts/toast-context.tsx
+++ src/contexts/toast-context.tsx
@@ -3,21 +3,15 @@ import Toast from '../components/common/Toast';
 import { ToastType } from '../types';
 
 export const ToastContext = createContext<{
-  setToasts: React.Dispatch<React.SetStateAction<ToastType[]>>;
   createToast: (toast: ToastType) => void;
   deleteToast: (id: string) => void;
 }>({
-  setToasts: () => {},
   createToast: () => {},
   deleteToast: () => {},
 });
 
 export function ToastProvider({ children }: { children: React.ReactNode }) {
-  const [toasts, setToasts] = useState<ToastType[]>([
-    { text: 'successfully done', type: 'success', id: 'asdf' },
-    { text: 'failed', type: 'fail', id: 'zxcv' },
-    { text: 'change your password', type: 'warning', id: 'qwer' },
-  ]);
+  const [toasts, setToasts] = useState<ToastType[]>([]);
 
   const createToast = (toast: ToastType) => {
     setToasts((prev) => [...prev, toast]);
@@ -28,7 +22,7 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
   };
 
   return (
-    <ToastContext.Provider value={{ setToasts, createToast, deleteToast }}>
+    <ToastContext.Provider value={{ createToast, deleteToast }}>
       <ul className="max-md:w-full md:max-w-[300px] md:w-full max-md:px-5 fixed max-md:bottom-5 right-1/2 max-md:translate-x-1/2 md:top-20 md:right-4 z-20 flex flex-col space-y-3">
         {toasts.map(({ text, type, id }) => (
           <Toast key={id} text={text} type={type} id={id} />
