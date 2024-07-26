3bbc0a02575f59ffce24c3cf6ca241a4007c0a93
Yonn-Yoo <85042907+Yonn-Yoo@users.noreply.github.com>
2024-07-26 17:23:16 +0900

    feat: create scrollToTop component for scrolling to top

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import { ToastProvider } from '../../contexts/toast-context';
 import { UserProvider } from '../../contexts/user-context';
 import usePath from '../../hooks/use-path';
+import ScrollToTop from '../common/ScrollToTop';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -11,6 +12,7 @@ export default function Layout() {
   return (
     <UserProvider>
       <ToastProvider>
+        <ScrollToTop />
         <Header />
         <main className={`flex-grow ${!isHome && 'pt-16'}`}>
           <Outlet />
